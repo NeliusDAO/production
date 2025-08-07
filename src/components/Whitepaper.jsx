cfebe509d74cff633bcc4ca9fea6277932cf3999
Temi Michael <olasehindemichael285@gmail.com>
2025-08-07 13:45:52 +0100

    Update : Fix responsiveness of buttons and interactivity

--- src/components/Whitepaper.jsx
+++ src/components/Whitepaper.jsx
@@ -1,36 +1,62 @@
-import React from "react"
+import React, { useState } from "react";
 import { useContext } from "react";
 import { ToggleContext } from "./ToggleContext";
-import Download from "../assets/img/logo/Download Document.png"
+import Download from "../assets/img/logo/Download Document.png";
 
 export default function Whitepaper() {
-    const { isToggled } = useContext(ToggleContext);
-    const dark = {
-        backgroundColor: "#010101",
-        color: "white",
-    }
+  const { isToggled } = useContext(ToggleContext);
+  const [isDownloaded, setIsDownloaded] = useState(false);
 
-    const text = {
-        color: 'white',
-        background: 'linear-gradient(-45deg, #FEFEFE, #02AFF3, #02AFF3, #FEFEFE, #FEFEFE)',
-        WebkitBackgroundClip: 'text',
-        WebkitTextFillColor: 'transparent',
-        backgroundClip: 'text',
-    };
+  const dark = {
+    backgroundColor: "#010101",
+    color: "white",
+  };
 
-    const handleDownload = () => {
-        const link = document.createElement('a');
-        link.download = 'Nelius_Whitepaper.pdf';
-        link.href = require('../assets/Nelius_Whitepaper.pdf');
-        link.click();
-    };
+  const text = {
+    color: "white",
+    background:
+      "linear-gradient(-45deg, #FEFEFE, #02AFF3, #02AFF3, #FEFEFE, #FEFEFE)",
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+    backgroundClip: "text",
+  };
 
-    return (
-        <div className="whitepaper" id="whitepaper" style={!isToggled ? dark : {}}>
-            <p className="whitepaperText" id="whitepaperText">Read <span style={!isToggled ? text : {}} className="textOne">the Nelius White Pap</span>er<br /><span style={!isToggled ? text : {}} className="textOne">for more informati</span>on</p>
-            <button className="whitepaperDownload" id="whitepaperDownload" onClick={handleDownload}>
-                <img src={Download} className="whiteimg" id="whiteimg" alt="download whitepaper" /> Download Document
-            </button>
-        </div>
-    );
-};
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.download = "Nelius_Whitepaper.pdf";
+    link.href = require("../assets/Nelius_Whitepaper.pdf");
+    link.click();
+    setIsDownloaded(true);
+  };
+
+  return (
+    <div className="whitepaper" id="whitepaper" style={!isToggled ? dark : {}}>
+      <p className="whitepaperText" id="whitepaperText">
+        Read{" "}
+        <span style={!isToggled ? text : {}} className="textOne">
+          the Nelius White Pap
+        </span>
+        er
+        <br />
+        <span style={!isToggled ? text : {}} className="textOne">
+          for more informati
+        </span>
+        on
+      </p>
+      <button
+        className="whitepaperDownload"
+        id="whitepaperDownload"
+        onClick={handleDownload}
+      >
+        <img
+          src={Download}
+          className="whiteimg"
+          id="whiteimg"
+          alt="download whitepaper"
+        />{" "}
+        Download Document
+      </button>
+      {isDownloaded && <p id="downloadText">Thank you for downloading the Nelius Whitepaper</p>}
+    </div>
+  );
+}
